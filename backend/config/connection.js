6408b61c5159821325077d5e7ff717c3a800c046
MarCrivelli <marcoantonio7crivelli@gmail.com>
2025-03-04 15:28:55 -0300

    Update 23
    Backend funcionando!

--- backend/config/connection.js
+++ backend/config/connection.js
@@ -9,14 +9,18 @@ const sequelize = new Sequelize(
     host: process.env.DB_HOST || 'localhost', // Host do banco
     dialect: 'postgres', // Tipo do banco
     port: process.env.DB_PORT || 5432, // Porta do banco
-    logging: false
-  });
+    logging: false // Desativa logs do Sequelize no console
+  }
+);
 
-try {
-  sequelize.authenticate();
-  console.log('Conectado com a base de dados!');
-} catch (error) {
-  console.error('Sem comunicação com a base de dados!', error);
-}
+// Função assíncrona para testar a conexão
+(async () => {
+  try {
+    await sequelize.authenticate(); // Aguarda a autenticação
+    console.log('Conectado com a base de dados!');
+  } catch (error) {
+    console.error('Sem comunicação com a base de dados!', error);
+  }
+})();
 
 module.exports = { Sequelize, sequelize };
