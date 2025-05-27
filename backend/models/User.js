c0e2868d77865781878381e8f3decc2e9d6e2abf
MarCrivelli <marcoantonio7crivelli@gmail.com>
2025-05-27 00:20:39 -0300

    Update 44
    Coloquei um pouco mais de informações na página de denúncias.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,25 +1,25 @@
-const connection = require('../config/connection');
+const connection = require("../config/connection");
 
-const User = connection.sequelize.define('db_user',{
-    id: {
-        type: connection.Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: {
-        type: connection.Sequelize.STRING,
-        allowNull: false
-    },
-    password:{
-        type: connection.Sequelize.STRING,
-        allowNull:false
-    },
-    email:{
-        type: connection.Sequelize.STRING,
-        allowNull:false,
-        unique:true
-    } 
-    })
+const User = connection.sequelize.define("db_user", {
+  id: {
+    type: connection.Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: {
+    type: connection.Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: connection.Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: connection.Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+  },
+});
 User.sync();
 module.exports = User;
