ddc2aa981853f4629fbbf69f1bc1385236f6458c
MarCrivelli <marcoantonio7crivelli@gmail.com>
2026-05-20 17:20:19 -0300

    Update 100
    Finalizei o design da página de postagem.

--- src/app/componentes/Administradores/PaginaDePostagem/app.jsx
+++ src/app/componentes/Administradores/PaginaDePostagem/app.jsx
@@ -11,7 +11,6 @@ import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 
 export default function ProgramarPostagem() {
-
   const [abaAtiva, setAbaAtiva] = useState("primeiroTopico");
 
   // Estados para os filtros selecionados
@@ -69,12 +68,14 @@ export default function ProgramarPostagem() {
           <div className={styles.introducaoPagina}>
             <h1>Programar postagem no Instagram / Facebook</h1>
             <p>
-              Faça postagens diretamente pelo site de maneira fácil e rápida! Logo abaixo estarão <span>&quot;Filtros pré-selecionados&quot;</span>{" "}
-              contém um formulário que permite que sejam feitas postagens com os
-              animais que estão cadastrados no site. Tudo que precisa ser feito
-              é selecionar os filtros pré-definidos que buscaram automaticamente
-              todos os animais que contém as características desejadas para a
-              postagem
+              Faça postagens diretamente pelo site de maneira fácil e rápida!
+              Logo abaixo estarão{" "}
+              <span>&quot;Filtros pré-selecionados&quot;</span> e{" "}
+              <span>&quot;Seleção Manual&quot;</span>. No bloco de filtros
+              pré-selecionados, você escolherá as características que os animais
+              que farão parte da postagem terão e qual a data que deseja postar.
+              Já no bloco de selecão manual, você mesmo selecionará os animais
+              que você deseja inserir na postagem.
             </p>
           </div>
           <Tabs
@@ -86,25 +87,155 @@ export default function ProgramarPostagem() {
           >
             <Tab
               eventKey="primeiroTopico"
-              title="Filtros pré-selecionados"
-              tabClassName={abaAtiva === "primeiroTopico" ? styles.botaoTabSelecionado : styles.botaoTabNaoSelecionado}
-              className={styles.conteudoTab}
+              title="Pré-seleção"
+              tabClassName={
+                abaAtiva === "primeiroTopico"
+                  ? styles.botaoTabSelecionado
+                  : styles.botaoTabNaoSelecionado
+              }
+              className={`${styles.conteudoTab} ${styles.conteudoTab1}`}
             >
-              <p>
-                Todo estado possui uma delegacia virtual na qual você pode
-                acessar para denunciar, no anonimato ou não, diferentes tipos de
-                situações, incluindo situações referentes a causa animal. Se
-                você não souber o link da delegacia de seu estado, selecione seu
-                estado na caixa abaixo:
-              </p>
-              
+              <div className={styles.containerFiltros}>
+                <div className={styles.filtro}>
+                  <label>Idade</label>
+                  <Select
+                    isMulti
+                    options={opcoesSelect.idadeAnimais}
+                    value={filtrosSelecionados.idade}
+                    onChange={handleChangeFiltro("idade")}
+                    placeholder="Selecione"
+                    className={styles.select}
+                  />
+                </div>
+
+                <div className={styles.filtro}>
+                  <label>Tipo de animal</label>
+                  <Select
+                    options={opcoesSelect.tipoAnimal}
+                    value={filtrosSelecionados.tipoAnimal}
+                    onChange={handleChangeFiltro("tipoAnimal")}
+                    placeholder="Selecione"
+                    className={styles.select}
+                  />
+                </div>
+
+                <div className={styles.filtro}>
+                  <label>Sexo</label>
+                  <Select
+                    options={opcoesSelect.sexoDoAnimal}
+                    value={filtrosSelecionados.sexo}
+                    onChange={handleChangeFiltro("sexo")}
+                    placeholder="Selecione"
+                    className={styles.select}
+                  />
+                </div>
+
+                <div className={styles.filtro}>
+                  <label>Status de vacinação</label>
+                  <Select
+                    options={opcoesSelect.StatusVacinacao}
+                    value={filtrosSelecionados.statusVacinacao}
+                    onChange={handleChangeFiltro("statusVacinacao")}
+                    placeholder="Selecione"
+                    className={styles.select}
+                  />
+                </div>
+
+                <div className={styles.filtro}>
+                  <label>Status de castração</label>
+                  <Select
+                    options={opcoesSelect.StatusCastracao}
+                    value={filtrosSelecionados.statusCastracao}
+                    onChange={handleChangeFiltro("statusCastracao")}
+                    placeholder="Selecione"
+                    className={styles.select}
+                  />
+                </div>
+
+                <div className={styles.filtro}>
+                  <label>Status de adoção</label>
+                  <Select
+                    options={opcoesSelect.StatusAdocao}
+                    value={filtrosSelecionados.statusAdocao}
+                    onChange={handleChangeFiltro("statusAdocao")}
+                    placeholder="Selecione"
+                    className={styles.select}
+                  />
+                </div>
+
+                <div className={styles.filtro}>
+                  <label>Status de microchipagem</label>
+                  <Select
+                    options={opcoesSelect.StatusMicrochipagem}
+                    value={filtrosSelecionados.statusMicrochipagem}
+                    onChange={handleChangeFiltro("statusMicrochipagem")}
+                    placeholder="Selecione"
+                    className={styles.select}
+                  />
+                </div>
+
+                <div className={styles.filtro}>
+                  <label>Status de vermifugação</label>
+                  <Select
+                    options={opcoesSelect.StatusVermifugacao}
+                    value={filtrosSelecionados.statusVermifugacao}
+                    onChange={handleChangeFiltro("statusVermifugacao")}
+                    placeholder="Selecione"
+                    className={styles.select}
+                  />
+                </div>
+
+                <div className={styles.filtro}>
+                  <label>Quando publicar?</label>
+                  <Select
+                    options={opcoesTempoPostagem}
+                    value={filtrosSelecionados.opcaoPublicacao}
+                    onChange={handleChangeTempoPostagem}
+                    placeholder="Selecione"
+                    className={styles.select}
+                  />
+
+                  {filtrosSelecionados.opcaoPublicacao?.value === "agendar" && (
+                    <div className={styles.seletorDataContainer}>
+                      <input
+                        type="date"
+                        name="dataInicio"
+                        value={filtrosSelecionados.dataPostagem}
+                        onChange={handleChangeData}
+                        required
+                      />
+                      <DatePicker
+                        selected={filtrosSelecionados.dataPostagem}
+                        onChange={handleChangeData}
+                        showTimeSelect
+                        timeFormat="HH:mm"
+                        timeIntervals={15}
+                        dateFormat="dd/MM/yyyy HH:mm"
+                        minDate={new Date()}
+                        placeholderText="Selecione data e hora"
+                        className={styles.seletorDataInput}
+                        withPortal
+                        isClearable
+                      />
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className={styles.containerBotaoPostagem}>
+                <button>Postar</button>
+              </div>
             </Tab>
             <Tab
               eventKey="segundoTopico"
               title="Seleção Manual"
-              tabClassName={abaAtiva === "segundoTopico" ? styles.botaoTabSelecionado : styles.botaoTabNaoSelecionado}
+              tabClassName={
+                abaAtiva === "segundoTopico"
+                  ? styles.botaoTabSelecionado
+                  : styles.botaoTabNaoSelecionado
+              }
               className={styles.conteudoTab}
             >
+              <div className={styles.conteudoTab2}>
                 <h1>Seleção Manual</h1>
                 <p>
                   Deseja ignorar os filtros e selecionar manualmente os animais
@@ -117,6 +248,7 @@ export default function ProgramarPostagem() {
                   </Link>{" "}
                   para começar a seleção.
                 </p>
+              </div>
             </Tab>
           </Tabs>
           {/* <div className={styles.introducaoPagina}>
