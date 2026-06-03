08d050abeeaa7a80a68e73a8bb0a15a6d6f586b8
MarCrivelli <marcoantonio7crivelli@gmail.com>
2026-06-03 12:27:35 -0300

    Update 108
    Fiz todos os tópicos da página de saúde única, agora só falta o design.

--- src/app/componentes/Visitantes/PaginaDeSaudeUnica/app.jsx
+++ src/app/componentes/Visitantes/PaginaDeSaudeUnica/app.jsx
@@ -1,8 +1,32 @@
 import Header from "../HeaderVisitantes/app";
 import Footer from "../Footer/app";
 import styles from "./saudeUnica.module.css";
+import { useEffect } from "react";
 
 export default function SaudeUnica() {
+  useEffect(() => {
+    const card = document.querySelectorAll(`.${styles.card}`);
+
+    const observador = new IntersectionObserver(
+      (entradas) => {
+        entradas.forEach((entrada) => {
+          const progresso = entrada.intersectionRatio;
+
+          entrada.target.style.setProperty("--progresso", progresso);
+        });
+      },
+      {
+        threshold: Array.from({ length: 101 }, (_, indice) => indice / 100),
+      },
+    );
+
+    card.forEach((cartao) => observador.observe(cartao));
+
+    return () => {
+      observador.disconnect();
+    };
+  }, []);
+
   return (
     <>
       <main>
@@ -18,97 +42,100 @@ export default function SaudeUnica() {
         >
           <div className={styles.painel}>
             <p>
-            A Saúde Única é uma abordagem integrada e interdisciplinar proposta
-            por organizações internacionais, como a Organização Mundial da Saúde
-            &#40;OMS&#41;, a Organização Mundial da Saúde Animal &#40;OIE&#41; e
-            a Organização das Nações Unidas para Alimentação e Agricultura
-            &#40;FAO&#41;, que reconhece que a saúde humana, a saúde animal e a
-            saúde ambiental estão interligadas. Ou seja, para garantir a saúde
-            das pessoas, é necessário também cuidar dos animais e do meio
-            ambiente.
-          </p>
+              A Saúde Única é uma abordagem integrada e interdisciplinar
+              proposta por organizações internacionais, como a Organização
+              Mundial da Saúde &#40;OMS&#41;, a Organização Mundial da Saúde
+              Animal &#40;OIE&#41; e a Organização das Nações Unidas para
+              Alimentação e Agricultura &#40;FAO&#41;, que reconhece que a saúde
+              humana, a saúde animal e a saúde ambiental estão interligadas. Ou
+              seja, para garantir a saúde das pessoas, é necessário também
+              cuidar dos animais e do meio ambiente.
+            </p>
           </div>
-          <img className={styles.foguete} src="/pagSaudeUnica/rocket.png"/>
-          <img className={styles.fogo} src="/pagSaudeUnica/fire.png"/>
+          <img className={styles.foguete} src="/pagSaudeUnica/rocket.png" />
+          <img className={styles.fogo} src="/pagSaudeUnica/fire.png" />
         </section>
 
         <section
           className={`${styles.secao} ${styles.parallax} ${styles.parallax2}`}
         >
-          <h1>Em quais ocasiões a saúde humana, animal e ambiental se interligam?</h1>
-        </section>
-        <section
-          className={`${styles.secao} ${styles.conteudo} ${styles.conteudo2}`}
-        ></section>
-      </main>
-      {/* <div className={styles.ajusteElementosDaPagina}>
-        <div className={styles.seccao}>
-          <h1>O que é Saúde Única?</h1>
-          <p>
-            A Saúde Única é uma abordagem integrada e interdisciplinar proposta
-            por organizações internacionais, como a Organização Mundial da Saúde
-            &#40;OMS&#41;, a Organização Mundial da Saúde Animal &#40;OIE&#41; e
-            a Organização das Nações Unidas para Alimentação e Agricultura
-            &#40;FAO&#41;, que reconhece que a saúde humana, a saúde animal e a
-            saúde ambiental estão interligadas. Ou seja, para garantir a saúde
-            das pessoas, é necessário também cuidar dos animais e do meio
-            ambiente.
-          </p>
-        </div>
-
-        <div className={styles.seccao}>
           <h1>
             Em quais ocasiões a saúde humana, animal e ambiental se interligam?
           </h1>
-          <p>
-            <strong className={styles.strong}>
-              1 - Propagação de doenças infecciosas:{" "}
-            </strong>{" "}
-            algumas doenças infecciosas como a Zoonose, Raiva ou Leptospirose,
-            por exemplo, não se limitam apenas a animais ou humanos, podendo ser
-            transmitida de um para o outro, sendo extremamente mortais caso não
-            haja um tratamento imediato.
-          </p>
-          <p>
-            <strong className={styles.strong}>2- Segurança alimentar: </strong>
-            como aprendemos desde cedo na escola, a saúde do ambiente em que
-            vivemos impacta diretamente na nossa saúde, bem como a dos animais.
-            Comidas de procedência duvidosa ou preparada sem higiene pode fazer
-            a nós e aos animais adoecer e até a ajudar a propagar outros tipos
-            de doenças, como a Doença da Vaca Louca, que matou várias pessoas
-            devido a falta de responsabilidade e conhecimento quanto a práticas
-            ambientais duvidosas e consequentemente matando diversas vacas,
-            muitas apenas pelo medo da contaminação.
-          </p>
-          <p>
-            <strong className={styles.strong}>3- Agressões: </strong> apesar de
-            ser normalizado por algumas pessoas, bater, espancar ou agredir o
-            animal de qualquer forma, causando lesões ou dor, é sim um motivo
-            para denúncia e pode fazer com que o dono perca a tutela do animal.
-          </p>
-          <p>
-            <strong className={styles.strong}>4- Exploração: </strong> quaisquer
-            tipos de exploração animal é motivo de denúncia. Neste caso, o termo
-            &quot;exploração&quot; pode se encaixar em: explorar para fins
-            lucrativos, não cuidando da saúde do animal e fazendo-o ir além de
-            seus limites &#40; um exemplo é o que acontece com animais que são
-            resgatados de círcos clandestinos &#41;. E explorar para fins
-            próprios, como realizar tarefas pesadas ou atos sexuais.
-          </p>
-          <p>
-            <strong className={styles.strong}>5- Envenenamento: </strong> dar de
-            propósito substâncias tóxicas, álcool, drogas, cigarro e outros é
-            mais do que um bom motivo para realizar uma denúncia, pois podem
-            causar convunções, falha nos órgãos e até a morte de animais.
-          </p>
-          <p>
-            <strong className={styles.strong}>6- Crueldade: </strong>{" "}
-            comportamentos cruéis, como machucar, afogar, ou até mesmo forçar
-            situações em que deixam o animal com medo, apenas por diversão,
-            também é passível de denúncia.
-          </p>
-        </div>
-      </div>*/}
+        </section>
+        <section
+          className={`${styles.secao} ${styles.conteudo} ${styles.conteudo2}`}
+        >
+          <div className={styles.card}>
+            <p>
+              <strong className={styles.strong}>
+                1 - Propagação de doenças infecciosas:{" "}
+              </strong>{" "}
+              algumas doenças infecciosas como a Zoonose, Raiva ou Leptospirose,
+              por exemplo, não se limitam apenas a animais ou humanos, podendo
+              ser transmitida de um para o outro, sendo extremamente mortais
+              caso não haja um tratamento imediato.
+            </p>
+          </div>
+          <div className={styles.card}>
+            <p>
+              <strong className={styles.strong}>
+                2 - Contaminação do lençol freático:{" "}
+              </strong>{" "}
+              Quando rios, poços ou nascentes recebem esgoto, lixo, agrotóxicos
+              ou fezes de animais e não recebem o devido tratamento, além de
+              prejudicar diretamente a natureza pela contaminação do solo e do
+              lençol freático, acaba por prejudicar a todos que dependem dessas
+              fontes para sobreviver: os humanos e os animais.
+            </p>
+          </div>
+          <div className={styles.card}>
+            <p>
+              <strong className={styles.strong}>
+                3 - Queimadas e suas consequências:{" "}
+              </strong>{" "}
+              As queimadas prejudicam a fauna, destroem vegetação, empobrecem o
+              solo e comprometem os pulmões de quem respira sua fumação por
+              tempo demais.
+            </p>
+          </div>
+          <div className={styles.card}>
+            <p>
+              <strong className={styles.strong}>
+                4 - Dengue, zika e chikungunya:{" "}
+              </strong>{" "}
+              O acúmulo de lixo, água parada e falta de limpeza ambiental
+              favorece a ploriferação do mosquito da dengue, zika e chikungunya,
+              que por sua vez são perigosos para humanos e animais também.
+            </p>
+          </div>
+          <div className={styles.card}>
+            <p>
+              <strong className={styles.strong}>
+                5 - Resíduos e lixo descartados de forma errada:{" "}
+              </strong>{" "}
+              a reciclagem e o descarte ideal para cada tipo de lixo são
+              fundamentais para preservar o meio-ambiente, pois evita que lixo
+              se acumule, atraindo insetos e animais que podem propagar doenças,
+              além de, claro, adoecer àqueles que convivem com ele diariamente.
+            </p>
+          </div>
+          <div className={styles.card}>
+            <p>
+              <strong className={styles.strong}>
+                6 - Desmatamento e perda de habitat:{" "}
+              </strong>{" "}
+              o desmatamento contribui para dois problemas sérios na saúde
+              única: quanto menos árvores, menor é a eficiência de filtragem do
+              CO², gerando danos à camada de ozônio e consequentemente,
+              superaquecendo o planeta; o desmatamento contribui também para a
+              migração de animais selvagens para as cidades, o que prejudica
+              tanto as pessoas, que podem ser atacadas, quanto os animais, que
+              podem ser atropelados ou caçados.
+            </p>
+          </div>
+        </section>
+      </main>
       <Footer />
     </>
   );
